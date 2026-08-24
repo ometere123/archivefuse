30bfc793124895e0a13eedea06f5e0d3a2ffb443
papitodele <45469370+ometere123@users.noreply.github.com>
2026-08-24 17:16:26 +0100

    feat: add ArchiveFuse contract

---
+++ contracts/archivefuse.py
@@ -0,0 +1,6 @@
+# {
+#   "Seq": [
+#     { "Depends": "py-lib-genlayer-embeddings:0bmbm3cyfwxsyh454z53vxqjf47wz2q7smcqp1q4g4a6k2kidnyk" },
+#     { "Depends": "py-genlayer:1jb45aa8ynh2a9c9xn3b7qqh8sm5q93hwfp7jqmwsfhh8jpz09h6" }
+#   ]
+# }
