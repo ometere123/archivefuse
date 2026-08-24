dd3936e295e2e340a9260f5a75ef796d60be0641
papitodele <45469370+ometere123@users.noreply.github.com>
2026-08-24 18:31:02 +0100

    chore: require hardened lifecycle methods

--- lib/genlayer/config.ts
+++ lib/genlayer/config.ts
@@ -11,9 +11,10 @@ export const explorerAddressUrl = (address: string) => `${EXPLORER_BASE}/address
 
 export const REQUIRED_METHODS = [
   "create_archive", "set_curator", "register_record", "preview_candidates",
-  "propose_resolution", "adjudicate_resolution", "get_archive", "get_record",
-  "get_case", "get_cluster", "is_curator", "get_record_cluster",
+  "propose_resolution", "adjudicate_resolution", "invalidate_stale_resolution",
+  "get_archive", "get_record", "get_case", "get_cluster", "is_curator", "get_record_cluster",
   "list_archive_ids", "list_record_ids", "list_case_ids", "list_cluster_members",
   "list_cluster_case_ids", "preview_correction_context", "propose_correction",
-  "adjudicate_correction", "get_correction", "list_correction_ids", "list_cluster_correction_ids", "stats",
+  "adjudicate_correction", "invalidate_stale_correction", "get_correction",
+  "list_correction_ids", "list_cluster_correction_ids", "stats",
 ] as const;
